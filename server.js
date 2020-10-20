73b97b3ea983398d57033b09196082c42cd76652
Raul Barrera <rbarrera1992@gmail.com>
2020-10-20 17:36:05 -0500

    complete

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 const movies = require('./movies-data.json');
 
-console.log(process.env.API_TOKEN);
+// console.log(process.env.API_TOKEN);
 
 const app = express();
 
